4191cf057c3b956ef224cc16fbd55cd0ac23c978
marslin <marslin@gmail.com>
2016-12-23 00:35:40 +0800

    The first answer.

--- AddTwoNumbers.playground/Contents.swift
+++ AddTwoNumbers.playground/Contents.swift
@@ -18,7 +18,42 @@ import Cocoa
  */
 class Solution {
     func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        var firstNumber = 0
+        var secondNumber = 0
+        var multiplyer = 1
+        var tempNode = l1
         
+        while tempNode != nil {
+            firstNumber += (tempNode?.val)! * multiplyer
+            multiplyer *= 10
+            tempNode = tempNode?.next
+        }
+        
+        tempNode = l2
+        multiplyer = 1
+        while tempNode != nil {
+            secondNumber += (tempNode?.val)! * multiplyer
+            multiplyer *= 10
+            tempNode = tempNode?.next
+        }
+        print(secondNumber)
+        
+        return numberToListNode(number: firstNumber + secondNumber)
+    }
+    
+    func numberToListNode(number: Int) -> ListNode? {
+        var transferNumber = number
+        
+        let sumNode = ListNode(transferNumber % 10)
+        var tempNode = sumNode
+        while transferNumber > 9 {
+            transferNumber = Int(transferNumber / 10)
+            let childNode = ListNode(transferNumber % 10)
+            tempNode.next = childNode
+            tempNode = childNode
+        }
+        
+        return sumNode
     }
 }
 
@@ -30,3 +65,8 @@ public class ListNode {
         self.next = nil
     }
 }
+
+var solution = Solution()
+var l1 = solution.numberToListNode(number: 243)
+var l2 = solution.numberToListNode(number: 564)
+solution.addTwoNumbers(l1, l2)
