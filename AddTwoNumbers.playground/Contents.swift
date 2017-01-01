909e1d347cb4c148f845eb40344cd9b2520a7da2
marslin <marslin@gmail.com>
2017-01-01 22:16:12 +0800

    Misunderstanding the question, fix it.

--- AddTwoNumbers.playground/Contents.swift
+++ AddTwoNumbers.playground/Contents.swift
@@ -18,29 +18,47 @@ import Cocoa
  */
 class Solution {
     func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        var firstNumber = 0
-        var secondNumber = 0
-        var multiplyer = 1
-        var tempNode = l1
         
-        while tempNode != nil {
-            firstNumber += (tempNode?.val)! * multiplyer
-            multiplyer *= 10
-            tempNode = tempNode?.next
+        var node1 = l1
+        var node2 = l2
+        let sumNode = ListNode(0)
+        var tempNode = sumNode
+        var quotient = 0
+        
+        while node1 != nil || node2 != nil {
+            if node1 != nil {
+                tempNode.val = (node1?.val)!
+            }
+            
+            if node2 != nil {
+                tempNode.val = tempNode.val + (node2?.val)!
+            }
+            
+            tempNode.val += quotient
+            
+            if (tempNode.val > 9) {
+                quotient = 1
+                tempNode.val = tempNode.val % 10
+            } else {
+                quotient = 0
+            }
+            
+            if node1?.next != nil || node2?.next != nil {
+                tempNode.next = ListNode(0)
+                tempNode = tempNode.next!
+            } else if quotient != 0 {
+                tempNode.next = ListNode(1)
+                tempNode = tempNode.next!
+            }
+            
+            node1 = node1?.next
+            node2 = node2?.next
         }
         
-        tempNode = l2
-        multiplyer = 1
-        while tempNode != nil {
-            secondNumber += (tempNode?.val)! * multiplyer
-            multiplyer *= 10
-            tempNode = tempNode?.next
-        }
-        print(secondNumber)
-        
-        return numberToListNode(number: firstNumber + secondNumber)
+        return sumNode
     }
     
+    // for testing only
     func numberToListNode(number: Int) -> ListNode? {
         var transferNumber = number
         
