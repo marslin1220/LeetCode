c9e0f47014c46e1a912aa7f8d43321bbd1287469
Cheng Lung Lin <marslin@gmail.com>
2019-08-26 02:11:40 +0800

    錯寫到 Sample page 了

--- LeetCode.playground/Pages/Sample.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/Sample.xcplaygroundpage/Contents.swift
@@ -1,34 +1,11 @@
 //: [Previous](@previous)
 
-//1. Two Sum
-//https://leetcode.com/problems/two-sum/
-
-// ref: https://github.com/soapyigu/LeetCode-Swift/blob/master/Array/TwoSum.swift
-//Runtime: 36 ms, faster than 68.68% of Swift online submissions for Two Sum.
-//Memory Usage: 21.2 MB, less than 5.88% of Swift online submissions for Two Sum.
-
 import XCTest
 
 class Solution {
-    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        var dict = [Int: Int]()
-
-        for (i, num) in nums.enumerated() {
-            if let lastIndex = dict[target - num] {
-                return [lastIndex, i]
-            }
-
-            dict[num] = i
-        }
-
-        fatalError("No valid outputs")
-    }
 }
 
 class MyTests : XCTestCase {
-    func test1() {
-        XCTAssertEqual(Solution().twoSum([2, 7, 11, 15], 9), [0, 1])
-    }
 }
 
 TestRunner().runTests(testClass: MyTests.self)
